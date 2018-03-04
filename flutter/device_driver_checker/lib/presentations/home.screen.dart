c7bc1ef47379707ee9be10a73358fe1159ccac40
Alexander Kazantsev <akazantsev@s-pro.io>
2018-03-04 16:20:27 +0200

    ddc: added statefull widget

--- flutter/device_driver_checker/lib/presentations/home.screen.dart
+++ flutter/device_driver_checker/lib/presentations/home.screen.dart
@@ -1,41 +1,51 @@
-import 'package:device_driver_checker/containers/devices-list.container.dart';
+import 'dart:io';
+
 import 'package:device_driver_checker/utils/keys.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen() : super(key: Keys.HOME_SCREEN);
+
+  @override
+  State<StatefulWidget> createState() => new HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    /** CHECK IF USER HAS A TOKEN AND VERIFY IT */
+    (() async {
+      final SharedPreferences state = await SharedPreferences.getInstance();
+      final String key = state.getString('token');
+      if (key == null) {
+        /** MOVE TO LOGIN SCREEN */
+        debugPrint('key is NULL');
+        sleep(new Duration(seconds: 5));
+        setState(() {
+          isLoading = false;
+        });
+      }
+    })();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-          title: new Text('Device Driver Checker'),
+          title: const Text('Device Driver Checker'),
         ),
-//        body: new DevicesList(key: Keys.DEVICES_LIST_CONTAINER),
+        body: new Center(
+          child: isLoading ? const Text('Loading...') : const Text('Loaded'),
+        ),
         floatingActionButton: new FloatingActionButton(
           key: Keys.ADD_TODO_FAB,
-          onPressed: _increment,
+          onPressed: () => debugPrint('PRESS'),
           tooltip: 'Add new device',
           child: new Icon(Icons.add),
         ));
   }
 }
-
-_increment() async {
-  SharedPreferences preferences = await SharedPreferences.getInstance();
-  int counter = (preferences.getInt('counter') ?? 0) + 1;
-  print('Pressed $counter times.');
-  preferences.setInt('counter', counter);
-}
-
-//class DDCState extends State<HomeScreen> {
-//  Future _prefs = SharedPreferences.getInstance();
-//  @override
-//  Widget build(BuildContext context) {
-//    return new Scaffold(
-//      appBar: new AppBar(title: const Text('Device Driver Checker'),),
-//    );
-//  }
-
-//}
